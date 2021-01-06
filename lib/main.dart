26c7141785b65927bec5349b12c50eed774a5ca4
11mrhappy <nekomamiremy@gmail.com>
2021-01-06 20:15:55 +0900

    icons, text, button,

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,65 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+       theme: ThemeData(
+         primaryColor: Colors.white,
+       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('reviewアプリ'),
+          centerTitle: false,
+          leading: Icon(Icons.videocam),
+          title: const Text(
+              'reviewアプリ',
+          ),
+          actions: [
+            SizedBox(
+              width: 44,
+              child: FlatButton(
+                child: Icon(Icons.search),
+                onPressed: (){
+
+                },
+              ),
+            ),
+            SizedBox(
+              width: 44,
+              child: FlatButton(
+                child: Icon(Icons.more_vert),
+                onPressed: (){
+
+                },
+              ),
+            ),
+          ],
         ),
-        body: Container(),
+        body: Container(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Column(
+                    children: [
+                      const Text(
+                        'yorks view',
+                      ),
+                      FlatButton(
+                        child: Row(
+                          children: [
+                            Icon(Icons.video_call),
+                            Text('登録する'),
+                          ],
+                        ),
+                        onPressed: (){
+
+                        },
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
